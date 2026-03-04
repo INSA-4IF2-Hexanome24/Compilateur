cd6bb1b67cc44ba46a62a3a92adaeaa9f4bb2cbc
LiamMiam <llt44@outlook.fr>
2026-03-04 08:52:32 +0100

    4.5-6

---
+++ compiler/SymbolTableVisitor.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <map>
+#include <set>
+#include <string>
+#include <iostream>
+#include "antlr4-runtime.h"
+#include "generated/ifccBaseVisitor.h"
+
+class SymbolTableVisitor : public ifccBaseVisitor {
+public:
+    // Maps variable name -> stack index (negative offset from %rbp, multiple of 4)
+    std::map<std::string, int> symbolTable;
+
+    // Returns false if any semantic error was found
+    bool success = true;
+
+    virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override;
+    virtual antlrcpp::Any visitDecl_stmt(ifccParser::Decl_stmtContext *ctx) override;
+    virtual antlrcpp::Any visitAssign_stmt(ifccParser::Assign_stmtContext *ctx) override;
+    virtual antlrcpp::Any visitReturn_stmt(ifccParser::Return_stmtContext *ctx) override;
+    virtual antlrcpp::Any visitVarExpr(ifccParser::VarExprContext *ctx) override;
+
+private:
+    int nextIndex = -4;           // next available stack slot
+    std::set<std::string> used;   // variables that have been read
+};
--- compiler/ifcc.g4
+++ compiler/ifcc.g4
@@ -2,12 +2,26 @@ grammar ifcc;
 
 axiom : prog EOF ;
 
-prog : 'int' 'main' '(' ')' '{' return_stmt '}' ;
+prog : 'int' 'main' '(' ')' '{' stmt* return_stmt '}' ;
 
-return_stmt: RETURN CONST ';' ;
+stmt
+    : decl_stmt
+    | assign_stmt
+    ;
+
+decl_stmt   : 'int' VAR ';' ;
+assign_stmt : VAR '=' expr ';' ;
+
+return_stmt : RETURN expr ';' ;
+
+expr
+    : CONST   # constExpr
+    | VAR     # varExpr
+    ;
 
 RETURN : 'return' ;
-CONST : [0-9]+ ;
-COMMENT : '/*' .*? '*/' -> skip ;
-DIRECTIVE : '#' .*? '\n' -> skip ;
-WS    : [ \t\r\n] -> channel(HIDDEN);
+CONST  : [0-9]+ ;
+VAR    : [a-zA-Z_][a-zA-Z_0-9]* ;
+COMMENT   : '/*' .*? '*/' -> skip ;
+DIRECTIVE : '#' .*? '\n'  -> skip ;
+WS        : [ \t\r\n]     -> channel(HIDDEN);
