b7978f1288fbad6997f76646404484b5b821c342
GarouxE <ewan.garoux44@gmail.com>
2026-03-20 11:38:22 +0100

    test if imbrique qui ne marche pas

--- testfiles/33_return_multiples.c
+++ testfiles/33_return_multiples.c
@@ -2,5 +2,5 @@ int main() {
     int x = 5;
     int y = 0;
     return y;
-    return
+    return;
 }
---
+++ testfiles/38_if_else_imbriques.c
@@ -0,0 +1,16 @@
+int main() {
+    int x = 1;
+    int y = 0;
+    if (x) { 
+        if(1){
+            x= 9;
+        }
+        else{
+            x=2;
+        }
+    } 
+    else { 
+        y = 2; 
+    }
+    return 3;
+}
